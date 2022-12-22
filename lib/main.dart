ae4893a000a7252922957963c7d613fd59ce69d4
liam <huyhomie66@gmail.com>
2022-12-22 11:39:13 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/theme.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/welcome/welcome_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      home: WelcomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/chats/chats_screen.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:chat_app/constants.dart';
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  _ChatsScreenState createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  int _selectedIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: kPrimaryColor,
+        child: const Icon(
+          Icons.person_add_alt_1,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selectedIndex,
+      onTap: (value) {
+        setState(() {
+          _selectedIndex = value;
+        });
+      },
+      // ignore: prefer_const_literals_to_create_immutables
+      items: [
+         const BottomNavigationBarItem(icon: Icon(Icons.messenger), label: "Chats"),
+         const BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
+         const BottomNavigationBarItem(icon: Icon(Icons.call), label: "Calls"),
+         const BottomNavigationBarItem(
+          icon: CircleAvatar(
+            radius: 14,
+            backgroundImage: AssetImage("assets/images/user_2.png"),
+          ),
+          label: "Profile",
+        ),
+      ],
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: const Text("Chats"),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
